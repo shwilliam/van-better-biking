1c19ab12c587b614383342c33461a56d29c44c6c
William L <w-lindvall@outlook.com>
2019-11-08 15:56:20 -0800

    refactor map component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-/*global mapboxgl*/
+import React from 'react'
+import Map from './components/Map'
+import './App.css'
 
-import React, {useEffect} from 'react'
-import './index.css'
-
-const App = () => {
-  useEffect(() => {
-    mapboxgl.accessToken = `pk.${process.env.REACT_APP_MAPBOX_KEY}`
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10',
-      center: [-122.662323, 45.523751], // starting position
-      zoom: 12,
-    })
-    // set the bounds of the map
-    const bounds = [
-      [-123.069003, 45.395273],
-      [-122.303707, 45.612333],
-    ]
-    map.setMaxBounds(bounds)
-
-    // initialize the map canvas to interact with later
-    const canvas = map.getCanvasContainer()
-
-    // an arbitrary start will always be the same
-    // only the end or destination will change
-    const start = [-122.662323, 45.523751]
-  }, [])
-  return <div id="map"></div>
-}
+const App = () => <Map />
 
 export default App
---
+++ src/components/Map/index.js
@@ -0,0 +1,32 @@
+/*global mapboxgl*/
+
+import React, {useEffect} from 'react'
+import './index.css'
+
+const Map = () => {
+  useEffect(() => {
+    mapboxgl.accessToken = `pk.${process.env.REACT_APP_MAPBOX_KEY}`
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10',
+      center: [-122.662323, 45.523751], // starting position
+      zoom: 12,
+    })
+    // set the bounds of the map
+    const bounds = [
+      [-123.069003, 45.395273],
+      [-122.303707, 45.612333],
+    ]
+    map.setMaxBounds(bounds)
+
+    // initialize the map canvas to interact with later
+    const canvas = map.getCanvasContainer()
+
+    // an arbitrary start will always be the same
+    // only the end or destination will change
+    const start = [-122.662323, 45.523751]
+  }, [])
+  return <div id="map"></div>
+}
+
+export default Map
