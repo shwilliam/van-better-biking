4344c98a7c45e6bc905d5d6e97741a92751bd255
William L <w-lindvall@outlook.com>
2019-11-09 15:06:03 -0800

    fix broken import

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,7 +1,6 @@
 /* global google */
 import React, {useContext, useLayoutEffect} from 'react'
 import {MapContext} from '../../context'
-import theftData from '../../data/theft'
 import './index.css'
 // import bikeRacks from './bike-racks'
 
@@ -295,7 +294,7 @@ const Map = props => {
       new google.maps.LatLng(49.2827, -123.1207),
       new google.maps.LatLng(49.2827, -123.1207),
       new google.maps.LatLng(49.2827, -123.1207),
-      new google.maps.LatLng(49.2827, -123.1207)
+      new google.maps.LatLng(49.2827, -123.1207),
     ]
 
     var gradient = [
@@ -312,17 +311,15 @@ const Map = props => {
       'rgba(63, 0, 91, 1)',
       'rgba(127, 0, 63, 1)',
       'rgba(191, 0, 31, 1)',
-      'rgba(255, 0, 0, 1)'
+      'rgba(255, 0, 0, 1)',
     ]
 
     let heatmap = new google.maps.visualization.HeatmapLayer({
-      data: heatmapData
+      data: heatmapData,
     })
-    heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+    heatmap.set('gradient', heatmap.get('gradient') ? null : gradient)
     heatmap.set('radius', heatmap.get('radius') ? null : 50)
 
-    
-  
     heatmap.setMap(gmap)
   }, [map, setMap, setDirectionsService, setDirectionsRenderer])
 
