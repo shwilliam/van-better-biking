063686ee0abca42926270bce594cbd7c70668ae3
William L <w-lindvall@outlook.com>
2019-11-08 20:56:25 -0800

    bye mapbox

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from 'react'
 import Map from './components/Map'
 import './App.css'
 
-const App = () => <Map />
+const App = () => (
+  <Map
+    googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_MAPS_KEY}&v=3.exp&libraries=geometry,drawing,places`}
+    loadingElement={<div style={{height: '100%'}} />}
+    containerElement={<div style={{height: '400px'}} />}
+    mapElement={<div style={{height: '100%'}} />}
+  />
+)
 
 export default App
--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,32 +1,18 @@
-/*global mapboxgl*/
-
-import React, {useEffect} from 'react'
+import React from 'react'
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+} from 'react-google-maps'
 import './index.css'
 
-const Map = () => {
-  useEffect(() => {
-    mapboxgl.accessToken = `pk.${process.env.REACT_APP_MAPBOX_KEY}`
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10',
-      center: [-122.662323, 45.523751], // starting position
-      zoom: 12,
-    })
-    // set the bounds of the map
-    const bounds = [
-      [-123.069003, 45.395273],
-      [-122.303707, 45.612333],
-    ]
-    map.setMaxBounds(bounds)
-
-    // initialize the map canvas to interact with later
-    const canvas = map.getCanvasContainer()
-
-    // an arbitrary start will always be the same
-    // only the end or destination will change
-    const start = [-122.662323, 45.523751]
-  }, [])
-  return <div id="map"></div>
-}
+const Map = withScriptjs(
+  withGoogleMap(props => (
+    <GoogleMap
+      defaultZoom={14}
+      center={{lat: 42.3601, lng: -71.0589}}
+    ></GoogleMap>
+  )),
+)
 
 export default Map
