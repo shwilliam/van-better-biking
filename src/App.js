c36c68a89c3ebe0de8d2889fb9b7d78e981f7c83
Roberto H <robhhr@outlook.com>
2019-11-10 01:07:28 -0800

    feat: style bike path

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import BikeRoutingForm from './components/BikeRoutingForm'
 import OverlayToggle from './components/OverlayToggle'
 import {MapContextProvider} from './context'
 import './App.css'
+import Weather from './components/Weather'
 
 const App = () => (
   <MapContextProvider>
@@ -12,6 +13,7 @@ const App = () => (
       style={{zIndex: 999, position: 'absolute', bottom: 0}}
     />
     <BikeRoutingForm style={{position: 'absolute', top: 0}} />
+    {/* <Weather /> */}
   </MapContextProvider>
 )
 
